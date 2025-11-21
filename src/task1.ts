81b119e0dd64376b2b75e0595982a7d5c851694b
WonderSi <kirillkozlov00@gmail.com>
2025-11-21 16:33:52 +0700

    feat: complete task1.ts

---
+++ src/task1.ts
@@ -0,0 +1,19 @@
+function formatInput(input: number): string;
+function formatInput(input: string): string;
+function formatInput(input: Date): string;
+
+function formatInput(input: number | string | Date): string {
+    if (typeof input === 'number') {
+        return `Number: ${input.toFixed(2)}`;
+    } else if (typeof input === 'string') {
+        return `String: ${input.toUpperCase()}`;
+    } else if (input instanceof Date) {
+        return `Date: ${input.toISOString()}`;
+    } else {
+        return 'Unknown'
+    }
+}
+
+console.log(formatInput(123.456));
+console.log(formatInput('World'));
+console.log(formatInput(new Date()));
